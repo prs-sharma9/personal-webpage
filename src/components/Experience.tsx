162dc554ca8b9f029ffbecbf2e82ad0ed18ac0b0
prashsharma88 <132094210+prs-sharma9@users.noreply.github.com>
2025-12-29 02:42:30 -0700

    Fixed the max width to 7xl

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -9,7 +9,7 @@ const Experience = () => {
         "Orchestrated a multidisciplinary team of six specialized engineers.",
         "Earned the prestigious Samsung India Innovation Award in 2018.",
       ],
-      position: "left"
+      position: "left",
     },
     {
       years: "Aug 2021 - Feb 2023",
@@ -20,7 +20,7 @@ const Experience = () => {
         "Enhanced platform performance by 25 percent via bottleneck analysis.",
         "Engineered a robust testing framework for multi-threaded systems.",
       ],
-      position: "right"
+      position: "right",
     },
     {
       years: "Aug 2023 - Nov 2023",
@@ -31,7 +31,7 @@ const Experience = () => {
         "Decreased UI-related bug reports by 70 percent within two sprints.",
         "Partnered cross-functionally to revamp legacy eCommerce components.",
       ],
-      position: "left"
+      position: "left",
     },
     {
       years: "Jan 2024 - Present",
@@ -42,50 +42,62 @@ const Experience = () => {
         "Designed curriculum modules achieving a 94 percent satisfaction rate.",
         "Integrated real-world challenges to boost application-based learning.",
       ],
-      position: "right"
+      position: "right",
     },
   ];
 
   return (
     <section id="experience-section">
       <div className="my-8 bg-primary/50 rounded-xl border border-primary/70 sm:p-10">
-        <div className="flex flex-col gap-3 justify-center items-center p-10">
-          <h2 className="font-semibold text-4xl bg-linear-to-b from-text-secondary/70 to-text-secondary/50 bg-clip-text text-transparent">
-            The Story So Far...
-          </h2>
-          <p className="text-text-primary/70">
-            14+ years of global experience in delivering cross functional
-            software solutions
-          </p>
-        </div>
+        <div className="max-w-7xl mx-auto">
+          <div className="flex flex-col gap-3 justify-center items-center p-10">
+            <h2 className="font-semibold text-4xl bg-linear-to-b from-text-secondary/70 to-text-secondary/50 bg-clip-text text-transparent">
+              The Story So Far...
+            </h2>
+            <p className="text-text-primary/70">
+              14+ years of global experience in delivering cross functional
+              software solutions
+            </p>
+          </div>
 
-        <div className="">
-          {exp_info.map((exp, key) => {
-            return (
-              <div key={key}>
-                <div className="mb-5">
-                  <div className="flex flex-col md:flex-row justify-between md:justify-center items-center">
-                    <div className={`flex-1 ${exp.position === 'left'? "md:order-1":"md:order-2"}`}>
-                      <div className="flex flex-col justify-center items-center">
-                        <p className="text-xl font-extrabold">{exp.company}</p>
-                        <p className="text-lg sm:text-sm md:text-md">
-                          {exp.years}
-                        </p>
+          <div className="">
+            {exp_info.map((exp, key) => {
+              return (
+                <div key={key}>
+                  <div className="mb-5">
+                    <div className="flex flex-col md:flex-row justify-between md:justify-center items-center">
+                      <div
+                        className={`flex-1 ${
+                          exp.position === "left" ? "md:order-1" : "md:order-2"
+                        }`}
+                      >
+                        <div className="flex flex-col justify-center items-center">
+                          <p className="text-xl font-extrabold">
+                            {exp.company}
+                          </p>
+                          <p className="text-lg sm:text-sm md:text-md">
+                            {exp.years}
+                          </p>
+                        </div>
                       </div>
-                    </div>
 
-                    <div className={`flex-1.5 ${exp.position === 'left'? "md:order-2":"md:order-1"}`}>
-                      <ul className="border bg-black/60 border-black/70 m-5 p-4 rounded-xl">
-                        {exp.highlight.map((item, itemKey) => {
-                          return <li key={itemKey}>✔︎ {item}</li>;
-                        })}
-                      </ul>
+                      <div
+                        className={`flex-1.5 ${
+                          exp.position === "left" ? "md:order-2" : "md:order-1"
+                        }`}
+                      >
+                        <ul className="border bg-black/60 border-black/70 m-5 p-4 rounded-xl">
+                          {exp.highlight.map((item, itemKey) => {
+                            return <li key={itemKey}>✔︎ {item}</li>;
+                          })}
+                        </ul>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
     </section>
